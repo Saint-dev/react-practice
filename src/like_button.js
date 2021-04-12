d670a84932254c9ae32b423db6936a34f2f5d40f
Saint <saint@cafealpha.dev>
2021-04-11 21:40:33 -0400

    updated files

--- src/like_button.js
+++ src/like_button.js
@@ -1,12 +1,22 @@
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
     );
-    ReactDOM.render(element, document.getElementById('root'));}
-  
-  setInterval(tick, 1000);
-  
-  
+  }
+}
+
+ReactDOM.render(<Toggle />, document.getElementById("root"));
